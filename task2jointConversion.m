acc82df62f8d176f0ee3ceeff99dd3c09d397ccc
Amejiyo Pakapak <amejiyopakapak@Amejiyos-MacBook-Air.local>
2021-11-24 14:03:58 +0700

    task2joint + inverseKine Fin

--- task2jointConversion.m
+++ task2jointConversion.m
@@ -1,9 +1,15 @@
-function [alert, via_point_joint] = task2jointConversion(via_point_task, command)
+function [alert, via_point_joint] = task2jointConversion(via_point_task)
 % check possible access + convert task to joint
-gamma = [1,1,-1,-1;1,-1,1,-1];
 via_point_joint = [];
 alert = 1;
-for i=1:length(gamma)
-    [q,alert] = IKHW2(via_point_task,gamma(i));
-
+[q,flag] = inverseKine(via_point_task);
+if all(flag==0)
+    alert = 0;
+    via_point_joint = q(:,:,1);
+else
+    column = find(flag==1);
+    for i=1:length(column)
+        via_point_joint(:,:,i) = q(:,:,column(i));
+    end
+end
 end
