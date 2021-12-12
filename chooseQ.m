4cf6f9056c33bfa2f25c9979a79aa0e6465ade19
Amejiyo Pakapak <amejiyopakapak@Amejiyos-MacBook-Air.local>
2021-12-12 15:43:34 +0700

    sa ti mai me leaw

--- chooseQ.m
+++ chooseQ.m
@@ -4,7 +4,6 @@ function [q_out,alert] = chooseQ(p,q_i)
 [flag,q] = task2jointConversion(p);
 p_pre = [100;100;100];
 alert = 0;
-s_q = size(q);
 for i=1:4
     if flag == 1
         if p_pre > norm(abs(q(:,i)-q_i))
